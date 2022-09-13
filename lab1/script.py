8e2d54896bd97bcaf9b879193ba1caba7eb709dd
houstonle <Houston.Le@hotmail.com>
2022-09-12 21:15:50 -0600

    inital commit

--- lab1/script.py
+++ lab1/script.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 response = requests.get('http://www.google.com/')
-github = requests.get('https://raw.githubusercontent.com/houstonle/CMPUT404-Labs/main/request_version.py')
+github = requests.get('https://raw.githubusercontent.com/houstonle/CMPUT404-Labs/main/script.py')
 print(github.text)
