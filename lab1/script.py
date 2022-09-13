2ae0329594297376336576a6d0c498fd1a2565d2
houstonle <Houston.Le@hotmail.com>
2022-09-12 21:20:00 -0600

    fixed github link

--- lab1/script.py
+++ lab1/script.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 response = requests.get('http://www.google.com/')
-github = requests.get('https://raw.githubusercontent.com/houstonle/CMPUT404-Labs/main/script.py')
+github = requests.get('https://raw.githubusercontent.com/houstonle/CMPUT404-Labs/main/lab1/script.py')
 print(github.text)
